2d00473cc6edfff9b8edaf898e401114d76638e8
Mark Howells-Mead <permanent.tourist@gmail.com>
2012-05-15 12:03:10 +0200

    hasInternetConnection.js v1.0

---
+++ Javascript/hasInternetConnection.js
@@ -0,0 +1,46 @@
+/*
+	free-standing javascript function to check whether the
+	client browser is currently connected to the internet.
+
+	navigator.onLine would be better, but isn't supported
+	problem-free in all browsers.
+
+	https://github.com/permanenttourist
+	v1.0 15.5.2012
+*/
+
+function hasInternetConnection=function(){
+	// thanks http://remysharp.com/2011/04/19/broken-offline-support/
+	// thanks http://www.javascriptkit.com/javatutors/trycatch.shtml
+
+	var onLine = false;
+	var ajaxrequest=null;
+	if (window.ActiveXObject){
+		try {
+			ajaxrequest=new ActiveXObject("Msxml2.XMLHTTP");
+		}catch(e){
+			try{
+				ajaxrequest=new ActiveXObject("Microsoft.XMLHTTP");
+			}catch(e){
+				alert("Ajax not supported in your browser!");
+			}
+		}
+	}else if (window.XMLHttpRequest){
+		ajaxrequest=new XMLHttpRequest();
+	}
+	ajaxrequest.open('HEAD','test.txt',false);
+	try {
+		ajaxrequest.send();
+		onLine = true;
+	}catch(e){
+		onLine = false;
+		alert('Please check your internet connection!');
+	}
+	return onLine;
+}//hasInternetConnection
+
+
+if(hasInternetConnection()){
+	//	do something which requires a live internet connection
+	//	e.g. make an ajax request to update some information
+}
